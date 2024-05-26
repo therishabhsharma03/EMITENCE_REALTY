775780e67b21cbba002e577b1ce2e77b0582732d
therishabhsharma03 <rishabh.sharma_2021@vitbhopal.ac.in>
2024-05-26 22:20:00 +0530

    crophecy

--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './style.css';
+import Home from './views/home';
+import NotFound from './views/not-found';
+import Log from './views/log';
+import Nav from './components/nav_and_footer/nav';
+import Guide from './components/guide/guide';
+import About from './views/about';
+// import "./i18n.js";
+import KidneyBeans_Info from './components/crops/KidneyBeans_Info';
+import ImageUploadForm from './views/Pridict';
+import Hero from './components/crops/crop';
+import Crop from './views/crops';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  // const selectedCrop = 'rice';
+  const validCrops = [`rice`,`wheat`, `kidneybeans`, `barley`,'mothbeans','chickpeas  ','pigeonpeas','maize','mungbean','blackgram','lentil','pomegranate','banana','mango','grapes','watermelon','muskmelon','apple','orange','papaya','coconut','cotton','jute','coffee'];
+  return (
+    <div className="App">
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/guide" component={Guide} exact />
+          <Route path="/log" component={Log} exact />
+          <Route path='/aboutus' component={About} />
+          <Route path='/predictor' component={ImageUploadForm} />
+          <Route path='/crops' component={Crop} />
+          {/* <Route path='/"kidneybeans"' component= {Hero cropName={selectedCrop}} /> */}
+          {validCrops.map((crop) => (
+            <Route key={crop} path={`/${crop}`}>
+              <Hero cropName={crop} />
+            </Route>
+          ))}
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render(<App />, document.getElementById('app'));
